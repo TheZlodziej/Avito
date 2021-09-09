62eea774a8e63da79402fee22ded9ad3e51f7586
Thezlodziej <galaxykuby@gmail.com>
2021-09-09 12:29:53 +0200

    SfFont -> Font (was not necessary in this file)

--- Avito/Assets/Assets.cs
+++ Avito/Assets/Assets.cs
@@ -1,6 +1,5 @@
 ﻿using SFML.Graphics;
 using System.IO;
-using SfFont = SFML.Graphics.Font;
 
 namespace Avito
 {
@@ -10,7 +9,7 @@ namespace Avito
         public static string AssetsDirectory { get; set; } = Path.Join(Settings.RootDirectory, "Assets");
 
         // FONTS
-        public static SfFont DefaultFont { get; set; } = new(Path.Join(AssetsDirectory, "Fonts\\default_font.ttf"));
+        public static Font DefaultFont { get; set; } = new(Path.Join(AssetsDirectory, "Fonts\\default_font.ttf"));
 
         // TEXTURES
         public static Texture PlayerTexture { get; set; } = new(Path.Join(AssetsDirectory, "Images\\test.png"));
