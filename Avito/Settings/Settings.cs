1b8e501c29a630290ac09e2bd83e33b744ca98ec
Thezlodziej <galaxykuby@gmail.com>
2021-09-15 00:22:18 +0200

    all settings properties are now only getters

--- Avito/Settings/Settings.cs
+++ Avito/Settings/Settings.cs
@@ -8,55 +8,55 @@ namespace Avito
     public static class Settings
     {
         // DIRECTORY
-        public static string RootDirectory { get; set; } = Directory.GetCurrentDirectory();
-        public static uint MaxFps { get; set; } = 120;
+        public static string RootDirectory { get; } = Directory.GetCurrentDirectory();
+        public static uint MaxFps { get; } = 120;
 
         // WINDOW
         public static class Window
         {
-            public static string Title { get; set; } = "Avito";
-            public static Vector2f Size { get; set; } = new(720, 520);
-            public static Vector2u USize { get; set; } = (Vector2u)Size;
-            public static Color ClearColor { get; set; } = new(100,100,100);
-            public static ContextSettings Settings { get; internal set; } = new() { AntialiasingLevel = 8 };
+            public static string Title { get; } = "Avito";
+            public static Vector2f Size { get; } = new(720, 520);
+            public static Vector2u USize { get; } = (Vector2u)Size;
+            public static Color ClearColor { get; } = new(100, 100, 100);
+            public static ContextSettings Settings { get; } = new() { AntialiasingLevel = 8 };
         }
 
         // HUD
-        public static class Hud 
+        public static class Hud
         {
-            public static Vector2f ItemSize { get; set; } = new(50, 50);
-            public static Vector2f ItemsBackgroundSize { get; set; } = new(ItemSize.X*Player.Inventory.Size, ItemSize.Y);
-            public static Color ItemsBackroundColor { get; set; } = new(0, 0, 0, 100);
-            public static Vector2f ItemsBackgroundPosition { get; set; } = new((Window.Size.X - ItemsBackgroundSize.X)/2f, Window.Size.Y - ItemsBackgroundSize.Y);
-            public static Color HpBarColor { get; set; } = new(230, 0, 38, 200);
-            public static Vector2f HpBarPosition { get; set; } = new(25, 25);
-            public static Vector2f HpBarSize { get; set; } = new(5, 30); // size of 1 hp (it's getting multiplied)
-            public static Color HpBarBackgroundColor { get; set; } = new(0, 0, 0, 100);
+            public static Vector2f ItemSize { get; } = new(50, 50);
+            public static Vector2f ItemsBackgroundSize { get; } = new(ItemSize.X * Player.Inventory.Size, ItemSize.Y);
+            public static Color ItemsBackroundColor { get; } = new(0, 0, 0, 100);
+            public static Vector2f ItemsBackgroundPosition { get; } = new((Window.Size.X - ItemsBackgroundSize.X) / 2f, Window.Size.Y - ItemsBackgroundSize.Y);
+            public static Color HpBarColor { get; } = new(230, 0, 38, 200);
+            public static Vector2f HpBarPosition { get; } = new(25, 25);
+            public static Vector2f HpBarSize { get; } = new(5, 30); // size of 1 hp (it's getting multiplied)
+            public static Color HpBarBackgroundColor { get; } = new(0, 0, 0, 100);
         }
 
         public static class Controls
         {
-            public static Keyboard.Key MoveLeft { get; set; } = Keyboard.Key.A;
-            public static Keyboard.Key MoveUp { get; set; } = Keyboard.Key.W;
-            public static Keyboard.Key MoveDown { get; set; } = Keyboard.Key.S;
-            public static Keyboard.Key MoveRight { get; set; } = Keyboard.Key.D;
+            public static Keyboard.Key MoveLeft { get; } = Keyboard.Key.A;
+            public static Keyboard.Key MoveUp { get; } = Keyboard.Key.W;
+            public static Keyboard.Key MoveDown { get; } = Keyboard.Key.S;
+            public static Keyboard.Key MoveRight { get; } = Keyboard.Key.D;
         }
 
         public static class Player
         {
-            public static int DefaultHp { get; set; } = 50;
-            public static float Mass { get; set; } = 10f;
+            public static int DefaultHp { get; } = 50;
+            public static float Mass { get; } = 10f;
 
             public static class Inventory
             {
-                public static int Size { get; set; } = 9;
+                public static int Size { get; } = 9;
             }
         }
 
         public static class Physics
         {
-            public static float FrictionCoeff { get; set; } = 0.998f;
-            public static float MoveForce { get; set; } = 10f;
+            public static float FrictionCoeff { get; } = 0.998f;
+            public static float MoveForce { get; } = 10f;
         }
     }
 }
